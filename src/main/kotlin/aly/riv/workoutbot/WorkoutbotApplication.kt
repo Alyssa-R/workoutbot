c69f6e0155f8bbfd44e8f438cb31c82829bbca67
Alyssa Rivera <arivera@everquote.com>
2021-01-07 17:27:00 -0500

    spring initializer

---
+++ src/main/kotlin/aly/riv/workoutbot/WorkoutbotApplication.kt
@@ -0,0 +1,11 @@
+package aly.riv.workoutbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkoutbotApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkoutbotApplication>(*args)
+}
---
+++ src/test/kotlin/aly/riv/workoutbot/WorkoutbotApplicationTests.kt
@@ -0,0 +1,12 @@
+package aly.riv.workoutbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkoutbotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
